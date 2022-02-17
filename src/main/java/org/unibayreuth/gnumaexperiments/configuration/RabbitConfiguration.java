2d21e042faf1a241b77b57355b2ca8e9b0478f77
Anatoly Obukhov <toll1605@gmail.com>
2022-02-17 19:28:01 +0100

    Unnecesarry import

--- src/main/java/org/unibayreuth/gnumaexperiments/configuration/RabbitConfiguration.java
+++ src/main/java/org/unibayreuth/gnumaexperiments/configuration/RabbitConfiguration.java
@@ -17,7 +17,6 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.unibayreuth.gnumaexperiments.handlers.exceptionhandling.ExceptionWrappingHandlerInterceptor;
 
 @Configuration
 public class RabbitConfiguration {
